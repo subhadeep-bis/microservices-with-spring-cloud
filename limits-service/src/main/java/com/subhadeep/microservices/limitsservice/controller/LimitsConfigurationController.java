0a5d1514dd4f5f820d74309f828c6a3a8fcb49c6
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-08-20 19:59:54 +0530

    Enhance limits service to get configuration from application properties

--- limits-service/src/main/java/com/subhadeep/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/subhadeep/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,14 +1,24 @@
 package com.subhadeep.microservices.limitsservice.controller;
 
 import com.subhadeep.microservices.limitsservice.bean.LimitConfiguration;
+import com.subhadeep.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsConfigurations() {
-        return new LimitConfiguration(1000, 1);
+        // Below syntax is for hardcoding limit values
+        //        return new LimitConfiguration(1000, 1);
+
+        // Below Syntax is used for taking limit values from application.properties
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+//        return new LimitConfiguration(configuration.getLimits().get("maximum"), configuration.getLimits().get("minimum"));
     }
 }
