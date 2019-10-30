e31913c83a2f71ed494d07892a65b7b6083fd7c4
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-10-30 23:00:01 +0530

    spring-config-server-git added for limits service

---
+++ spring-config-server-git/src/test/java/com/github/subhadeepbis/springconfigservergit/SpringConfigServerGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.subhadeepbis.springconfigservergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
