0a14000a9184f1002f530f560b33ee33ee6c5539
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-11-01 01:52:00 +0530

    JPA configured

--- currency-exchange-service/src/main/java/com/github/subhadeepbis/currencyexchangeservice/model/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/github/subhadeepbis/currencyexchangeservice/model/ExchangeValue.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 @Data
@@ -12,9 +16,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 public class ExchangeValue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal conversionMultiple;
     private Integer port;
 }
