6a7546d5caf66ade9cec4b151aeeecb63625da96
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-11-02 20:25:53 +0530

    setting currency-conversion-service

---
+++ currency-conversion-service/src/test/java/com/github/subhadeepbis/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.subhadeepbis.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
