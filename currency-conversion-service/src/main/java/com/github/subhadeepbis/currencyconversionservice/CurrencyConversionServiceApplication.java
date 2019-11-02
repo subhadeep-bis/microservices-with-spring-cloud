8699952f5eefcef7c9f7f25cda11052fa4af71ed
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-11-02 21:26:02 +0530

    final changes

--- currency-conversion-service/src/main/java/com/github/subhadeepbis/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/github/subhadeepbis/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,6 +2,8 @@ package com.github.subhadeepbis.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
@@ -10,4 +12,8 @@ public class CurrencyConversionServiceApplication {
         SpringApplication.run(CurrencyConversionServiceApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
--- currency-conversion-service/src/main/java/com/github/subhadeepbis/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/github/subhadeepbis/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,21 +1,28 @@
 package com.github.subhadeepbis.currencyconversionservice.controller;
 
 import com.github.subhadeepbis.currencyconversionservice.model.CurrencyConversionBean;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/currency-converter")
 public class CurrencyConversionController {
 
+    @Autowired
     private Environment environment;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     public CurrencyConversionController(Environment environment) {
@@ -25,8 +32,24 @@ public class CurrencyConversionController {
     @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable(value = "from") String from,
                                                   @PathVariable(value="to") String to,
-                                                  @PathVariable(value="quantity") String quantity) {
-        String port = environment.getProperty("local.server.port");
-        return new CurrencyConversionBean(1000L, from, to, BigDecimal.valueOf(75), BigDecimal.valueOf(Integer.parseInt(quantity)), BigDecimal.valueOf(75000), Integer.parseInt(port));
+                                                  @PathVariable(value="quantity") BigDecimal quantity) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        /**
+         * "uriVariables" below is used to fetch the values that are to be replaced in the uri (http://localhost:8002...) eg, {from} will be searched
+         * as a key in the uriVariables Map and since we have a key in the Map, it will fetch and assign it directly to the uri
+         * http://localhost:8002/currency-exchange-service/from/{from}/to/{to}
+         * if from = USD and to =  INR then the URI would look something like this
+         * http://localhost:8002/currency-exchange-service/from/USD/to/INR
+         * This method returns a ResponseEntity. If you would have used getForObject() then it would have returned an Object.
+         */
+        ResponseEntity<CurrencyConversionBean> responseEntity = restTemplate.getForEntity(
+                "http://localhost:8002/currency-exchange-service/from/{from}/to/{to}",
+                CurrencyConversionBean.class, uriVariables);
+        CurrencyConversionBean body = responseEntity.getBody();
+        body.setQuantity(quantity);
+        body.setTotalCalculatedAmount(quantity.multiply(body.getConversionMultiple()));
+        return body;
     }
 }
