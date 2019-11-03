77625e0150041749bd7b6144165ce25b9a4728c1
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-11-04 02:40:06 +0530

    netflix-eureka-naming-server added and all the previous services have been configured with eureka-config-client

--- currency-conversion-service/src/main/java/com/github/subhadeepbis/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/github/subhadeepbis/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,12 +2,14 @@ package com.github.subhadeepbis.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.github.subhadeepbis.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
