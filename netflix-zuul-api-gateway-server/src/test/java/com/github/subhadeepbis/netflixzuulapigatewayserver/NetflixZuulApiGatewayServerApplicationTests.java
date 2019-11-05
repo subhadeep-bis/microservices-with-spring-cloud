b2c858f4f145cfd1c229d079ac0120c7684cf11d
SuBhadeep BiSwas <subhadeepbiswas250@gmail.com>
2019-11-06 01:53:05 +0530

    netflix zuul api gateway server created and configured

---
+++ netflix-zuul-api-gateway-server/src/test/java/com/github/subhadeepbis/netflixzuulapigatewayserver/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.subhadeepbis.netflixzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
